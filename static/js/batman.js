84bff87abe3d8a9fb2da694846a6856db3aa640d
Xuyang Han <hanxuyang0826@gmail.com>
2019-02-12 13:59:40 -0500

    Create batman.js

---
+++ static/js/batman.js
@@ -0,0 +1,5 @@
+function SubmitSuccess() {
+    alert(" Your information has been submitted!" +
+        "\n Your name: " + document.getElementById("name").value +
+        "\n Your Email: " + document.getElementById("email").value);
+}

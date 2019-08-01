cf82bde8e56fd5bbee9904d9ff5d976e45fc9030
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2019-08-01 16:17:47 -0400

    updates

--- research/DB_Import.py
+++ research/DB_Import.py
@@ -5,18 +5,26 @@ import sys, getopt, pprint
 from pymongo import MongoClient
 
 # CSV to JSON Conversion
-csvfile = open(r'C:\Users\62707\Downloads\AIS_ASCII_by_UTM_Month\2017_v2\AIS_2017_01_Zone03.csv', 'r')
+csvfile = open(r'C:\Users\admin\Downloads\data downloaded\AIS_ASCII_by_UTM_Month\2017_v2\AIS_2017_01_Zone15.csv', 'r')
 reader = csv.DictReader(csvfile)
-mongo_client = MongoClient()
-db = mongo_client.trajectories_test
-db.AIS_2017_01_Zone03.drop()
+mongo_client = MongoClient(host='csb-comren.eng.yorku.ca', port=27017,
+                           username='admin',
+                           password='pse212')
+db = mongo_client.trajectories
+db.AIS_2017_01_Zone15.drop()
 header = ["MMSI", "BaseDateTime", "LAT", "LON", "SOG", "COG", "Heading",
           "VesselName", "IMO", "CallSign", "VesselType", "Status", "Length", "Width",
           "Draft", "Cargo"]
 
+i = 1
 for each in reader:
     row = {}
     for field in header:
         row[field] = each[field]
 
-    db.segment.insert(row)
+    db.AIS_2017_01_Zone15.insert(row)
+
+    if i % 1000 == 0:
+        print(i)
+
+    i = i + 1
---
+++ research/merge.py
@@ -0,0 +1,18 @@
+import os
+import csv
+
+csv_dir = r'C:\Users\admin\Downloads\data downloaded'
+os.chdir(csv_dir)
+
+with open('combined_data.csv', 'w', newline='') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerow(["Latitude", "Longitude", "Depth"])
+
+    with open('bathymetric data 1.csv', 'r', newline='') as incsv:
+        reader = csv.reader(incsv)
+        writer.writerows(row for row in reader)
+
+    with open('bathymetric data 2.csv', 'r', newline='') as incsv:
+        reader = csv.reader(incsv)
+        writer.writerows(row for row in reader)
+

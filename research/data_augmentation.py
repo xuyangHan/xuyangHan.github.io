2fe13357fbbbe2ffdad664d357e78b0c51c5dfe5
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2019-11-20 11:49:54 -0500

    updates

---
+++ research/data_augmentation.py
@@ -0,0 +1,50 @@
+import csv
+import pandas as pd
+
+csvfile = open('Check_Points.csv')
+reader = csv.DictReader(csvfile)
+
+with open('shifted_checkpoints2.csv', 'w') as csvfile:
+    fieldnames = ["MMSI", "Longitude", "Latitude", "SOG", "COG", "Heading"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    i = 1
+    for row in reader:
+        if i % 10000 == 0:
+            print(i)
+        i = i + 1
+
+        try:
+            Latitude = float(row["Latitude"]) - 0.01
+        except ValueError:
+            continue
+        try:
+            Longitude = float(row["Longitude"]) - 0.05
+        except ValueError:
+            continue
+        try:
+            MMSI = int(row["MMSI"])
+        except ValueError:
+            MMSI = row["MMSI"]
+        # if 43.14480 < Latitude < 44.38381 and -79.96854 < Longitude < -75.88100:
+        # if MMSI == 316013007:
+        # if i < 125:
+
+        try:
+            SOG = float(row["SOG"])
+        except ValueError:
+            SOG = row["SOG"]
+        try:
+            COG = float(row["COG"])
+        except ValueError:
+            COG = row["COG"]
+        try:
+            Heading = float(row["Heading"])
+        except ValueError:
+            Heading = row["Heading"]
+        writer.writerow({'MMSI': MMSI, "Longitude": Longitude, "Latitude": Latitude, "SOG": SOG, "COG":COG, "Heading":Heading})
+
+
+print('Data Loading finished.')
+

3d8db6c91190d9042867b391de68f52f5742e2cc
Xuyang Han <hanxuyang0826@gmail.com>
2021-03-23 22:24:16 -0400

    add new page

--- static/js/batman.js
+++
@@ -1,5 +0,0 @@
-function SubmitSuccess() {
-    alert(" Your information has been submitted!" +
-        "\n Your name: " + document.getElementById("name").value +
-        "\n Your Email: " + document.getElementById("email").value);
-}
